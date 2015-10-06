999d80b0e4af502a1445f4d7fd7d3758c7338d9d
Matthew Kleinsasser <mkleinsasser@digitalriver.com>
2015-10-06 14:45:32 -0500

    depends mysql, nginx, hhvm

--- blag/metadata.rb
+++ blag/metadata.rb
@@ -5,3 +5,8 @@ license 'all_rights'
 description 'Installs/Configures blag'
 long_description 'Installs/Configures blag'
 version '0.1.0'
+
+depends 'mysql', '~> 6.1.2'
+depends 'nginx', '~> 2.7.6'
+depends 'hhvm', '~> 0.6.0'
+

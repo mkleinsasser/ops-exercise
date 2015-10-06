b7b04af771880f92cb858504ed96e583762c0eb8
Matthew Kleinsasser <mkleinsasser@digitalriver.com>
2015-10-06 15:28:35 -0500

    include blag-mysql in blag::default

--- blag-mysql/recipes/default.rb
+++ blag-mysql/recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+
+mysql_service 'blag' do
+  port '3306'
+  version '5.6'
+  initial_root_password 'change me'
+  action [:create, :start]
+end

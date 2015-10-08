f83b2f8ab024e343acf620558a57525cc081285d
Matthew Kleinsasser <mkleinsasser@digitalriver.com>
2015-10-07 19:20:09 -0500

    stub some serverspec tests

--- cookbooks/blag/test/integration/default/serverspec/default_spec.rb
+++ cookbooks/blag/test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,56 @@
 require 'spec_helper'
 
-describe 'blag::default' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
-  end
+
+# Data tier
+
+describe service('mysql') do
+  it { should be_running }
+end
+
+describe port(3306) do
+  it { should be_listening.on('127.0.0.1').with('tcp') }
+end
+
+
+# Web tier
+
+describe service('hhvm') do
+  it { should be_running }
+end
+
+describe service('nginx') do
+  it { should be_running }
+end
+
+describe port(80) do
+  it { should be_listening.on('0.0.0.0').with('tcp') }
+end
+
+
+# Functionally test web server
+
+describe package('curl') do
+  it { should be_installed }
+end
+
+describe command('curl --head http://localhost/wp-admin/install.php') do
+  its(:stdout) { should match /HTTP\/1.1 200 OK/ }
+  its(:stdout) { should match /Server: nginx/ }
+  its(:stdout) { should match /X-Powered-By: HHVM/ }
+end
+
+
+# Users / Groups
+
+describe group('ops') do
+  it { should exist }
+end
+
+describe user('matt') do
+  it { should belong_to_group 'ops' }
+end
+
+describe command('sudo su - matt -c "sudo /usr/sbin/nginx -t"') do
+  its(:stderr) { should match /nginx.conf syntax is ok/ }
+  its(:stderr) { should match /nginx.conf test is successful/ }
 end
